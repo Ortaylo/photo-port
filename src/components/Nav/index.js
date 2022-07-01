6a9a71b4ebb5ed07f33ff588d8ffd609c29b3515
Owen Taylor <Owentaylor60@yahoo.com>
2022-07-01 14:22:44 -0400

    added PhotoList compnent and tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -27,6 +27,7 @@ function Nav(props) {
           <li className="mx-2">
             <a
               href="#about"
+              data-testid="about"
             >
               About me
             </a>
